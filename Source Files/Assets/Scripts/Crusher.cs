34169f03fe504bb49008115ad95cdafdc7b7816d
Graithen <grey.jim@gmail.com>
2018-06-28 18:07:31 +0100

    added crusher, and started work on saw bench

---
+++ Source Files/Assets/Scripts/Crusher.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crusher : MonoBehaviour {
+
+	bool goingUp = false;
+	public float currentSpeed;
+	float posY;
+	float lowestHeight;
+	float highestHeight;
+	float startHeight;
+
+	// Use this for initialization
+	void Start () {
+		startHeight = transform.localPosition.y;
+		highestHeight = startHeight;
+		lowestHeight = highestHeight - 8;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		posY = transform.localPosition.y;
+		Movement ();
+	}
+
+	void Movement() {
+
+		if (goingUp == true)
+		{
+			if ( posY >= highestHeight)
+			{
+				goingUp = false;
+			}
+		}
+		else
+		{
+			if (posY <= lowestHeight)
+			{
+				goingUp = true;
+			}
+		}
+
+		if ( goingUp == true)
+		{ 
+			transform.Translate(new Vector2 (0, currentSpeed * Time.deltaTime));
+		}
+		else
+		{
+			transform.Translate(new Vector2(0, -currentSpeed * Time.deltaTime));
+		}
+
+
+	}
+
+}
---
+++ Source Files/Assets/Scripts/SawBenchBlade.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawBenchBlade : MonoBehaviour {
+
+	public float minSpeed;
+	public float maxSpeed;
+	float currentSpeed;
+	float distance = 12f;
+
+	float posX;
+	float posY;
+
+	float furthestLeft;
+	float furthestRight;
+	float startPosition;
+
+	bool goingUp = coinFlip();
+	bool moveY;
+
+	// Use this for initialization
+	void Start () {
+		posX = transform.localPosition.x;
+
+		furthestRight = transform.localPosition.x;
+		furthestLeft = transform.localPosition.x + distance;
+		startPosition = Random.Range(furthestRight, furthestLeft);
+
+		Debug.Log(furthestRight + " ... " + furthestLeft);
+		currentSpeed = Random.Range (minSpeed, maxSpeed);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		posX = transform.localPosition.x;
+		Movement();
+	}
+
+	public static bool coinFlip (){
+		return(Random.value > 0.5f);
+	}
+
+	void Movement() {
+		if (moveY == true) {
+			//move Y
+		}
+
+		if (goingUp == true) {
+			if (posX >= furthestLeft) {
+				goingUp = false;
+			}
+		} else {
+			if (posX <= furthestRight) {
+				goingUp = true;
+			}
+		}
+
+		if (goingUp == true) { 
+			transform.Translate (new Vector2 (currentSpeed * Time.deltaTime,0));
+		} else {
+			transform.Translate (new Vector2 (-currentSpeed * Time.deltaTime,0));
+		}
+	}
+}
